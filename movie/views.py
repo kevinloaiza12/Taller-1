677971f068acce9a5e9f2c4593f072c5e4c56137
Kevin <kmloaizaa@eafit.edu.co>
2022-02-10 11:10:50 -0500

    cambio de nombre

--- movie/views.py
+++ movie/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html', {'name' : 'Greg Lim'})
+    return render(request, 'home.html', {'name' : 'Kevin Loaiza'})
 # Create your views here.
