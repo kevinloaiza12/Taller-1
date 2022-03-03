f20e66c9755517665c73e053558a7d155627612c
Kevin <kmloaizaa@eafit.edu.co>
2022-03-03 09:58:28 -0500

    Formulario de busqueda

--- movie/views.py
+++ movie/views.py
@@ -2,5 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'home.html', {'name' : 'Kevin Loaiza'})
-# Create your views here.
+    #return render(request, 'home.html', {'name' : 'Kevin Loaiza'})
+    searchTerm = request.GET.get('searchMovie')
+    return render(request, 'home.html', {'searchTerm':searchTerm})
+    
+def about (request):
+    return HttpResponse('<h1>Welcome to About Page</h1>')
